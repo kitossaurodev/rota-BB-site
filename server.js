acfac30eb2d9b1e630edf4278a63fef1a4551eac
kitossaurodev <marco.russo@blossomboostagency.com>
2025-06-23 20:40:05 -0300

    Update server.js
    voltando

--- server.js
+++ server.js
@@ -1,18 +1,10 @@
 require("dotenv").config();
 const express = require("express");
 const bodyParser = require("body-parser");
-const cors = require("cors"); // <-- AQUI
 const formRoutes = require("./src/routes/formRoutes");
 
 const app = express();
 const PORT = process.env.PORT || 3000;
-
-// Permitir CORS para o domínio do frontend
-app.use(cors({
-  origin: "https://blossomboost.com.br", // ou "*" para todos, com cuidado
-  methods: ["POST", "GET", "OPTIONS"],
-  allowedHeaders: ["Content-Type"]
-}));
 
 // Middleware para parsear JSON
 app.use(bodyParser.json());
@@ -24,4 +16,3 @@ app.use("/api", formRoutes);
 app.listen(PORT, () => {
   console.log(`Servidor rodando em http://localhost:${PORT}`);
 });
-
