5d0f5e4dc19bbbba2106a7c28570fe1940a72bf0
Roberto Felipe <feliperoberto092@gmail.com>
2025-06-23 22:02:15 -0300

    .

--- server.js
+++ server.js
@@ -2,9 +2,20 @@ require("dotenv").config();
 const express = require("express");
 const bodyParser = require("body-parser");
 const formRoutes = require("./src/routes/formRoutes");
+const cors = require("cors");
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+
+app.use(
+  cors({
+    origin: [
+      "https://blossomboost.com.br",
+    ],
+    methods: ["GET", "POST", "PUT"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+);
 
 // Middleware para parsear JSON
 app.use(bodyParser.json());
